cd266322a4e789daede3159ecafdebea000fcea4
Artem Kaloev <kaloev.eltech@gmail.com>
2018-07-16 20:13:31 +0300

    updated history section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,30 @@
-
 // подключение самописных модулей
 // import search from './modules/search';
 // search();
 
-// подключение файлов jquery скриптов как модулей
 import 'ion-rangeslider/css/ion.rangeSlider.css';
 import 'ion-rangeslider/css/ion.rangeSlider.skinHTML5.css';
 import 'ion-rangeslider/js/ion.rangeSlider.min.js';
 
 // подключение овл-карусели
-// import 'owl.carousel/dist/assets/owl.carousel.min.css';
-// import 'owl.carousel/dist/assets/owl.theme.default.min.css';
-// import 'owl.carousel';
+import 'owl.carousel/dist/assets/owl.carousel.min.css';
+import 'owl.carousel/dist/assets/owl.theme.default.min.css';
 
-//активация горизонтального слайдера
-$('#price-slider').ionRangeSlider({
-    min: 0,
-    max: 20000,
-    from: 1000,
-    to: 9000,
-    type: 'int',
-    grid_margin: false,
-    hide_min_max: true,
-    hide_from_to: true,
+import 'owl.carousel';
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        dots: false,
+        nav: true,
+        // navClass: '.owl-nav',
+        items: 1,
+        loop: true,
+        navigation: true, 
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        singleItem: true,
+    });
+    
 });
