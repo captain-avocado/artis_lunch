0f107cd2109f7cacef360677341efde1d1e6ae7a
Artem Kaloev <kaloev.eltech@gmail.com>
2018-07-18 12:31:14 +0300

    fixed validation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -49,8 +49,9 @@ $('#order').click(function(e) {
 
 $.validator.addMethod('customphone', function (phone_number, element) {
     phone_number = phone_number.replace(/\s+/g, ''); 
-    return this.optional(element) || 
-		phone_number.match(/^\+?[78][-(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/);
+    console.log(phone_number.length);
+    return this.optional(element) || (phone_number.length !== 0 &&
+		phone_number.match(/^\+?[78][-(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/));
 });
 
 
@@ -64,7 +65,10 @@ $(document).ready(function(){
                 required: true,
                 minlength: 2,
             },
-            phone: 'customphone',
+            phone: {
+                required: true,
+                'customphone': true,
+            },
             mail: {
                 required: true,
                 email: true,
