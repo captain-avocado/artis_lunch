9b13292b7520082f8fff57f154a108afb64df371
Artem Kaloev <kaloev.eltech@gmail.com>
2018-07-19 13:09:20 +0300

    updated map

--- src/scripts/modules/initMap.js
+++ src/scripts/modules/initMap.js
@@ -1,16 +1,29 @@
 export const initMap = function(google) {
-    const mapCenter = {lat: 59.927270, lng: 30.336667};
+    const mapCenter = {lat: 59.96356301, lng: 30.26885067};
+    const markerPlace = {lat: 59.9597891, lng: 30.266498};
 
-    new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
+    const map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
         center: mapCenter,
-        // styles: mapStyle,
-        mapTypeControl: false,
         zoomControl: true,
+        mapTypeControl: false,
         zoomControlOptions: {
             position: google.maps.ControlPosition.LEFT_CENTER,
         },
-        scaleControl: false,
-        fullscreenControl: false,
+    });
+
+    const popupContent = '<p class="content">Петровский проспект, 20 <br/> Метро Чкаловская/Спортивная</p>';
+
+    const marker = new google.maps.Marker({
+        position: markerPlace,
+        map: map,
+    });
+
+    const infoWindow = new google.maps.InfoWindow({
+        content: popupContent,
+    });
+    // infoWindow.open(map, marker);
+    marker.addListener('click', function() {
+        infoWindow.open(map, marker);
     });
 };
