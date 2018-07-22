fe9780ff9b7cf1afbe6d13d3636ea5315ca00b60
Artem Kaloev <kaloev.eltech@gmail.com>
2018-07-22 14:22:17 +0300

    updated form alert

--- src/scripts/modules/_formValidation.js
+++ src/scripts/modules/_formValidation.js
@@ -1,10 +1,11 @@
 import 'jquery-validation';
+import swal from 'sweetalert';
+
 
 export default function _formValidation() {
 
     $.validator.addMethod('customphone', function (phone_number, element) {
         phone_number = phone_number.replace(/\s+/g, ''); 
-        console.log(phone_number.length);
         return this.optional(element) || (phone_number.length !== 0 &&
 		phone_number.match(/^\+?[78][-(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/));
     });
@@ -26,6 +27,33 @@ export default function _formValidation() {
         },
         errorPlacement: function(){
             return false;
+        },
+        submitHandler: function(form) {
+
+            //delete 
+            swal('Спасибо', 'Мы скоро с вами свяжемся', 'success');
+            $('#form').trigger('reset');
+            //enddelete
+
+            $(form).submit(function() {
+                const data = {};
+                data['name'] = $('.input_name').val();
+                data['phone'] = $('.input_phone').val();
+                data['mail'] = $('.input_mail').val();
+                data['comment'] = $('.input_comment').val();
+        
+                const csrf_token = $('#form [name="csrfmiddlewaretoken"]').val();
+                data['csrfmiddlewaretoken'] = csrf_token;
+                $.ajax({
+                    type: 'POST',
+                    url: 'dinner',
+                    data: data,
+                }).done(function () {
+                    swal('Спасибо', 'Мы скоро с вами свяжемся', 'success');
+                    $('#form').trigger('reset');
+                });
+                return false;
+            });         
         },
     });
 }
